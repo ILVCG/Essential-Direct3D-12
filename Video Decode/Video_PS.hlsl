11bb6a396ac7d6cdbd2d7bcfa7eb7624bdf0fddc
Alex Wren <1062558943@qq.com>
2026-08-18 14:08:51 +0800

    Delete Video Decode directory

--- Video Decode/Video_PS.hlsl
+++
@@ -1,7 +0,0 @@
-Texture2D tex0 : register(t0);
-SamplerState samp0 : register(s0);
-
-float4 main(float4 pos : SV_POSITION, float2 uv : TEXCOORD0) : SV_Target
-{
-    return tex0.Sample(samp0, uv);
-}
